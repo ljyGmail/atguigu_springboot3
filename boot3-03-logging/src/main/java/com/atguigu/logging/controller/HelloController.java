3227a46e57483849c4a06906e78de2cc3f8390ee
liangjinyong <liangjy881@gmail.com>
2025-05-11 18:47:34 +0900

    019 日志 日志级别

--- boot3-03-logging/src/main/java/com/atguigu/logging/controller/HelloController.java
+++ boot3-03-logging/src/main/java/com/atguigu/logging/controller/HelloController.java
@@ -20,9 +20,14 @@ public class HelloController {
 //    Logger logger = LoggerFactory.getLogger(getClass());
 
     @GetMapping("/h")
-    public String hello() {
-        log.info("哈哈哈，方法进来了");
-//        logger.info("哈哈哈，方法进来了");
+    public String hello(String a, String b) {
+        log.trace("trace 日志...");
+        log.debug("debug 日志...");
+        // SpringBoot底层默认的日志级别 info
+        log.info("info 日志... 参数a: {}, b: {}", a, b);
+        log.warn("warn 日志...");
+        log.error("error 日志...");
+
         return "hello";
     }
 }
