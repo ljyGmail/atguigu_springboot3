935e90516d5b78fa66014df0c305aa6fa4884815
ljyGmail <95560385+ljyGmail@users.noreply.github.com>
2025-05-12 11:18:06 +0900

    022 日志 归档与切割

--- boot3-03-logging/src/main/java/com/atguigu/logging/controller/HelloController.java
+++ boot3-03-logging/src/main/java/com/atguigu/logging/controller/HelloController.java
@@ -21,12 +21,14 @@ public class HelloController {
 
     @GetMapping("/h")
     public String hello(String a, String b) {
-        log.trace("trace 日志...");
-        log.debug("debug 日志...");
-        // SpringBoot底层默认的日志级别 info
-        log.info("info 日志... 参数a: {}, b: {}", a, b);
-        log.warn("warn 日志...");
-        log.error("error 日志...");
+        for (int i = 0; i < 1000; i++) {
+            log.trace("trace 日志...");
+            log.debug("debug 日志...");
+            // SpringBoot底层默认的日志级别 info
+            log.info("info 日志... 参数a: {}, b: {}", a, b);
+            log.warn("warn 日志...");
+            log.error("error 日志...");
+        }
 
         return "hello";
     }
